5c7e26accd0c0c6d12d4126cf76d526a6475c2d3
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-06 20:36:00 -0600

    initial commit

---
+++ src/main/java/sv/edu/ues/bilbiotecaprojectbackend/BilbiotecaProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.bilbiotecaprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbiotecaProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilbiotecaProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/edu/ues/bilbiotecaprojectbackend/BilbiotecaProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.bilbiotecaprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbiotecaProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
